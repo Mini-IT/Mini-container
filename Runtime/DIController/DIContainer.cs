309437594f4acb6c96cac3906fb92dafe70f0c7a
Alexey Tsoy <alexey.tsoy@hitapps.com>
2023-11-02 01:46:32 +0600

    -test

--- Runtime/DIController/DIContainer.cs
+++ Runtime/DIController/DIContainer.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
-using UnityEngine; 
+using UnityEngine;
 
 namespace MiniContainer
 {
